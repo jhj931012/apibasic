3696d9962ea88954a6ca101215bfa08829436e6c
jhj <jungheejae931012@gmail.com>
2023-01-13 17:12:15 +0900

    스프링 API

---
+++ src/main/java/com/example/apibasic/post/api/PostApiController.java
@@ -0,0 +1,132 @@
+package com.example.apibasic.post.api;
+
+import com.example.apibasic.post.dto.*;
+import com.example.apibasic.post.entity.PostEntity;
+import com.example.apibasic.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
+// 리소스 : 게시물 (Post)
+/*
+     게시물 목록 조회:  /posts       - GET
+     게시물 개별 조회:  /posts/{id}  - GET
+     게시물 등록:      /posts       - POST
+     게시물 수정:      /posts/{id}  - PATCH
+     게시물 삭제:      /posts/{id}  - DELETE
+ */
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostApiController {
+
+    // PostRepository에게 의존하는 관계
+    private final PostRepository postRepository;
+
+    //@Autowired // 스프링 컨테이너에게 의존객체를 자동주입해달라
+//    public PostApiController(PostRepository postRepository) {
+//        this.postRepository = postRepository;
+//    }
+
+
+    // 게시물 목록 조회
+    @GetMapping
+    public ResponseEntity<?> list() {
+        log.info("/posts GET request");
+        List<PostEntity> list = postRepository.findAll();
+
+        // 엔터티 리스트를 DTO리스트로 변환해서 클라이언트에 응답
+        List<PostResponseDTO> responseDTOList = list.stream()
+                .map(PostResponseDTO::new)
+                .collect(toList());
+
+        PostListResponseDTO listResponseDTO = PostListResponseDTO.builder()
+                .count(responseDTOList.size())
+                .posts(responseDTOList)
+                .build();
+
+        return ResponseEntity
+                .ok()
+                .body(listResponseDTO)
+                ;
+    }
+
+    // 게시물 개별 조회
+    @GetMapping("/{postNo}")
+    public ResponseEntity<?> detail(@PathVariable Long postNo) {
+        log.info("/posts/{} GET request", postNo);
+
+        PostEntity post = postRepository.findOne(postNo);
+
+        // 엔터티를 DTO로 변환
+        PostDetailResponseDTO dto = new PostDetailResponseDTO(post);
+
+        return ResponseEntity
+                .ok()
+                .body(dto)
+                ;
+    }
+
+    // 게시물 등록
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody PostCreateDTO createDTO) {
+        log.info("/posts POST request");
+        log.info("게시물 정보: {}", createDTO);
+
+        // dto를 entity변환 작업
+        PostEntity entity = createDTO.toEntity();
+
+        boolean flag = postRepository.save(entity);
+        return flag
+                ? ResponseEntity.ok().body("INSERT-SUCCESS")
+                : ResponseEntity.badRequest().body("INSERT-FAIL")
+                ;
+    }
+
+    // 게시물 수정
+    @PatchMapping("/{postNo}")
+    public ResponseEntity<?> modify(
+            @PathVariable Long postNo
+            , @RequestBody PostModifyDTO modifyDTO
+    ) {
+        log.info("/posts/{} PATCH request", postNo);
+        log.info("수정할 정보 : {}", modifyDTO);
+
+        // 수정 전 데이터 조회하기
+        PostEntity entity = postRepository.findOne(postNo);
+        // 수정 진행
+        String modTitle = modifyDTO.getTitle();
+        String modContent = modifyDTO.getContent();
+
+        if (modTitle != null) entity.setTitle(modTitle);
+        if (modContent != null) entity.setContent(modContent);
+        entity.setModifyDate(LocalDateTime.now());
+
+        boolean flag = postRepository.save(entity);
+        return flag
+                ? ResponseEntity.ok().body("MODIFY-SUCCESS")
+                : ResponseEntity.badRequest().body("MODIFY-FAIL")
+                ;
+    }
+
+    // 게시물 삭제
+    @DeleteMapping("/{postNo}")
+    public ResponseEntity<?> remove(@PathVariable Long postNo) {
+        log.info("/posts/{} DELETE request", postNo);
+        boolean flag = postRepository.delete(postNo);
+        return flag
+                ? ResponseEntity.ok().body("DELETE-SUCCESS")
+                : ResponseEntity.badRequest().body("DELETE-FAIL")
+                ;
+    }
+}
---
+++ src/main/java/com/example/apibasic/post/entity/PostEntity.java
@@ -0,0 +1,33 @@
+package com.example.apibasic.post.entity;
+
+import com.example.apibasic.post.dto.PostResponseDTO;
+import com.example.apibasic.post.repository.PostRepository;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+// 게시물의 데이터 자바빈즈
+@Setter @Getter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class PostEntity {
+
+    public static long squence = 1L; // 연속된 일련번호
+
+    private Long postNo; // 게시물 식별번호
+    private String writer; // 작성자
+    private String title; // 제목
+    private String content; // 내용
+    private List<String> hashTags; // 해시태그 목록
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createDate; // 작성 시간
+    private LocalDateTime modifyDate; // 수정 시간
+
+
+
+}
