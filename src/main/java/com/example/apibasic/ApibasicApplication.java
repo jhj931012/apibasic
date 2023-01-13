b71ffb75ea197014a7f285effb91a32ef0b9ba05
jhj <jungheejae931012@gmail.com>
2023-01-13 12:14:33 +0900

    스프링 기초

---
+++ src/main/java/com/example/apibasic/ApibasicApplication.java
@@ -0,0 +1,16 @@
+package com.example.apibasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibasicApplication {
+
+	public static void main(String[] args) {
+		// 메롱메롱
+		int a = 10;
+		System.out.println();
+		SpringApplication.run(ApibasicApplication.class, args);
+	}
+
+}
